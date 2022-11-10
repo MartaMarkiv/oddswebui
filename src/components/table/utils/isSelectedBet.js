d6743bc460ee33523928dceda50de27b765b21fb
Marta Markiv <mpat@keplercode.com>
2022-11-10 11:29:06 +0200

    src/components/table/utils/isSelectedBet.js

---
+++ src/components/table/utils/isSelectedBet.js
@@ -0,0 +1,11 @@
+export const isSelectedBet = (selected, bet) => {
+    const betId = `${bet.game} - ${bet.betName}`
+    const isSelectedGame = selected.id === betId;
+    if (isSelectedGame) {
+        const betInfo = selected[bet.key];
+        const isSelectedBet = betInfo.book === bet.book && betInfo.betName === bet.betName && betInfo.value === bet.value;
+        return isSelectedBet;
+    }
+    return false;
+    
+};
