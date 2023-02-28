f09473d5fa3512fe497c96fb3878758a76f4d892
Evan Shoshan <evan.shoshan@gmail.com>
2023-02-27 21:46:31 -0500

    Update configuration values

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,16 @@
+const devConfig = {
+    table_data: process.env.REACT_APP_TABLE_DATA,
+    opportunity: process.env.REACT_APP_OPPORTUNITY
+}
+const prodConfig = {
+    table_data: "wss://user:Oddsbender@backend.oddsbender.com/ws/table_data",
+    opportunity: "wss://user:Oddsbender@backend.oddsbender.com/ws/opportunity_feed"
+} 
+
 export const DRAWER_WIDTH = 400;
 export const MULTI_DRAWER_WIDTH = 800;
-export const TABLE_DATA = process.env.REACT_APP_TABLE_DATA;
-export const OPPORTUNITY= process.env.REACT_APP_OPPORTUNITY;
+export const TABLE_DATA = process.env.NODE_ENV === "production" ? prodConfig.table_data : devConfig.table_data
+export const OPPORTUNITY= process.env.NODE_ENV === "production" ? prodConfig.opportunity : devConfig.opportunity
 
 export const QUARTERS_LIST = {
     q1: ["q1", "1st"],
