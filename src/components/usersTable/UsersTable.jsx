50a51b38b60518890f0fc6dfe9691f1fbed07c9b
Marta Markiv <mpat@keplercode.com>
2023-03-13 13:55:29 +0200

    Separated components

--- src/components/usersTable/UsersTable.jsx
+++ src/components/usersTable/UsersTable.jsx
@@ -5,6 +5,7 @@ import { IpRuleCell } from "./components/IpRuleCell";
 import { TableRow } from "./components/TableRow/TableRow";
 import { ActionBlock } from "./components/ActionBlock";
 import { ConfirmWindow } from "../ConfirmWindow";
+import { generateColumns } from "./utils/generateColumns";
 
 export const UsersTable = ({
 	users,
@@ -16,6 +17,15 @@ export const UsersTable = ({
 
 	const [data, setData] = useState(null);
 	const [isOpenConfirm, setIsOpenConfirm] = useState(false);
+
+	const actionRenderer = (title, type, handler, data) => (<ActionBlock
+		title={title}
+		type={type}
+		action={handler}
+		data={data}
+	/>);
+
+	const sessionRenderer = (data) => (<SessionWrap>{data}</SessionWrap>);
 
 	const confirmAction = () => {
 		deleteUser(data);
@@ -38,99 +48,14 @@ export const UsersTable = ({
       };
 
 
-    const columns = [
-        {
-			title: "User Id",
-			dataIndex: "userId",
-			key: "userId",
-        },
-        {
-			title: "Name",
-			dataIndex: "name",
-			key: "name",
-        },
-        {
-			title: "Email",
-			dataIndex: "email",
-			key: "email",
-			width: 200
-        },
-        {
-			title: "Status",
-			dataIndex: "status",
-			key: "status",
-			render: (_, {email, status}) => {
-				return status ?
-				<ActionBlock
-					title="Active"
-					type="active"
-					action={toggleBlockStatus}
-					data={{email, value: 0}}
-				/> :
-				<ActionBlock
-					title="Inactive"
-					type="inactive"
-					action={toggleBlockStatus}
-					data={{email, value: 1}}
-				/>
-          }
-        },
-        {
-			title: "IP Address",
-			dataIndex: "ipAddress",
-			key: "ipAddress",
-        },
-        {
-			title: "IP rule",
-			dataIndex: "ipRule",
-			key: "ipRule",
-			onCell: (record) => ({
-				record,
-				editable: true,
-				handleSave,
-			}),
-        },
-        {
-			title: "Location",
-			dataIndex: "location",
-			key: "location",
-        },
-        {
-			title: "Session time",
-			dataIndex: "session",
-			key: "session",
-			render: (_, { session }) => {
-				return <SessionWrap>{session || "-"}</SessionWrap>
-			},
-        },
-        {
-			title: "Reset password",
-			dataIndex: "resetPassword",
-			key: "resetPassword",
-			render: (_, { email }) => (
-				<ActionBlock
-				type="reset"
-				action={resetPassword}
-				data={{email}}
-				/>
-			),
-			className: "userAction"
-        },
-        {
-			title: "Remove user",
-			dataIndex: "removeUser",
-			key: "removeUser",
-			className: "userAction",
-			render: (_, {email}) => (
-				<ActionBlock
-				type="remove"
-				action={handleDelete}
-				data={{email}}
-				/>
-			),
-        },
-          
-      ];
+	const columns = generateColumns({
+		actionRenderer,
+		sessionRenderer,
+		toggleStatusHandler: toggleBlockStatus,
+		saveRuleHandler: handleSave,
+		resetHandler: resetPassword,
+		deleteHandler: handleDelete
+	});
     
       const components = {
         body: {
