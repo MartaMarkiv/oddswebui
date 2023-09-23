43945e576c717b1baa5f80f7a9738b357357c1e8
Raymond Gibbs <raymndgibbs@gmail.com>
2023-09-23 12:17:17 +0700

    feat(): update ui for auth cognito

--- src/components/opportunity/OpportunityDrawer/OpportunityDrawer.jsx
+++ src/components/opportunity/OpportunityDrawer/OpportunityDrawer.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { w3cwebsocket as WebSocket } from "websocket";
 import { notification } from "antd";
 import { DrawerStyled } from "./styles";
@@ -9,8 +8,7 @@ import { OPPORTUNITY_POPULAR, OPPORTUNITY_PROP } from "../../../constants";
 import { parser, arbitrageFilter } from "../utils";
 import { OpportunityWrapper } from "../OpportunityWrapper";
 import { useCurrentUser } from "../../../shared/context/UserProvider";
-import Cookies from 'universal-cookie';
-const cookies = new Cookies();
+import  useAuthenticated from "../../../shared/hooks/useAuthenticated";
 
 export const OpportunityDrawer = ({
     isProp,
@@ -21,37 +19,46 @@ export const OpportunityDrawer = ({
     const [collectionPopular, setCollectionPopular] = useState([]);
     const [collectionProp, setCollectionProp] = useState([]);
 
-    const { currentUser: { token: userToken }, setCurrentUser } = useCurrentUser();
-    const navigate = useNavigate();
+    const { handleLogout, loggedInUser, setUserToken } = useAuthenticated();
+    const { userToken, refreshToken } = loggedInUser() || {}
 
-    const clientPopular = new WebSocket(`${OPPORTUNITY_POPULAR}?token=${userToken}`);
-    const clientProp = new WebSocket(`${OPPORTUNITY_PROP}?token=${userToken}`);
+
+    const paramsString = `token=${userToken}&refresh_token=${refreshToken}`;
+    const clientPopular = new WebSocket(`${OPPORTUNITY_POPULAR}?${paramsString}`);
+    const clientProp = new WebSocket(`${OPPORTUNITY_PROP}?${paramsString}`);
 
     const [loading, setLoading] = useState(true);
+
+
+    const emitClientData = (event) => {
+        setLoading(false);
+
+        const json = JSON.parse(event.data);
+        const { new_access_token: newAccessToken, data } = json;
+
+        if (newAccessToken) {
+            setUserToken(newAccessToken);
+        }
+        if (!json.success && json.message && userToken) {
+            openNotification(json.message);
+            if (json.message === "Expired token" ||
+                json.message === "Unauthorized user" ||
+                json.message === "User is blocked"
+            ) {
+                handleLogout();
+            }
+        }
+
+        const allOpportunities = data.length ? data.map(item => item.games).flat() : [];
+        const parsedData = parser(allOpportunities);
+
+        return parsedData;
+    }
 
     const connectPopualrSocket = () => {
 
         clientPopular.onmessage = (event) => {
-            setLoading(false);
-           
-            const json = JSON.parse(event.data);
-            if (!json.success && json.message && userToken) {
-                openNotification(json.message);
-                if (json.message === "Expired token" ||
-                    json.message === "Unauthorized user" ||
-                    json.message === "User is blocked"
-                ) {
-                    cookies.remove("userBenderToken", { path: '/' });
-                    cookies.remove("userBenderRole", { path: '/' });
-                    setCurrentUser(null);
-                    navigate("/", { replace: true});
-                }
-            }
-            
-            const allOpportunities = json.length ? json.map(item => item.games).flat() : [];
-
-            const parsedData = parser(allOpportunities);
-
+            const parsedData = emitClientData(event);
             setCollectionPopular(parsedData);
         };
 
@@ -63,21 +70,7 @@ export const OpportunityDrawer = ({
     const connectPropSocket = () => {
 
         clientProp.onmessage = (event) => {
-            setLoading(false);
-            const json = JSON.parse(event.data);
-            if (!json.success && json.message) {
-                openNotification(json.message);
-                if (json.message === "Expired token") {
-                    cookies.remove("userBenderToken", { path: '/' });
-                    cookies.remove("userBenderRole", { path: '/' });
-                    setCurrentUser(null);
-                    navigate("/", { replace: true});
-                }
-            }
-            
-            const allOpportunities = json.length ? json.map(item => item.games).flat() : [];
-
-            const parsedData = parser(allOpportunities);
+                        const parsedData = emitClientData(event);
 
             setCollectionProp(parsedData);
         };
---
+++ src/shared/hooks/useAuthenticated.js
@@ -0,0 +1,57 @@
+
+import { useNavigate } from "react-router-dom";
+import Cookies from 'universal-cookie';
+import { useCurrentUser } from "../context/UserProvider";
+
+const cookies = new Cookies();
+export default function useAuthenticated() {
+    const navigate = useNavigate();
+    const { currentUser, setCurrentUser } = useCurrentUser();
+
+    const handleLogin = ({data, saveUser, rememberMe}) => {
+        delete data.success
+        const { token, role, refresh_token  } = data;
+        saveUser(data);
+        cookies.set('userBenderToken', token, { path: '/' });
+        cookies.set('userBenderRefreshToken', refresh_token, { path: '/' });
+        cookies.set('userBenderRole', role, { path: '/' });
+
+        storeRememberMe(rememberMe, data.email);
+    }
+    const handleLogout = () => {
+        cookies.remove("userBenderToken", { path: "/" });
+        cookies.remove("userBenderRefreshToken", { path: "/" });
+        cookies.remove("userBenderRole", { path: "/" });
+        setCurrentUser(null);
+        navigate("/", { replace: true });
+    }
+
+    const loggedInUser = () => {
+        let userToken = currentUser?.token || cookies.get('userBenderToken');
+        let refreshToken = currentUser?.refresh_token || cookies.get('userBenderRefreshToken');
+
+        return { userToken, refreshToken };
+    }
+
+    const setUserToken = (token) => {
+        cookies.set('userBenderToken', token, { path: '/' });
+        setCurrentUser({...currentUser, token});
+    }
+
+    const storeRememberMe = (rememberMe, email) => {
+        if (rememberMe) {
+            cookies.set('rememberMe', rememberMe, { path: '/' });
+            cookies.set('userBenderEmail', email, { path: '/' });
+        } else {
+            cookies.remove('rememberMe', { path: '/' });
+            cookies.remove('userBenderEmail', { path: '/' });
+        }
+    }
+
+    return {
+        handleLogin,
+        handleLogout,
+        loggedInUser,
+        setUserToken
+    }
+}
